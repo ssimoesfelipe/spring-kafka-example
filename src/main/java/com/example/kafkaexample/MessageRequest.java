5c75a92893ebbc4541053c54b3efc63c6d98d846
ssimoesfelipe <ssimoes.felipe@gmail.com>
2022-04-06 22:07:41 -0300

    add files

---
+++ src/main/java/com/example/kafkaexample/MessageRequest.java
@@ -0,0 +1,14 @@
+package com.example.kafkaexample;
+
+public class MessageRequest {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
